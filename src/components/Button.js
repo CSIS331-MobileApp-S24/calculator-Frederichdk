e8cdf0b511f17e1ba2fb1a630bd9687dac4a5df2
frederichdk <fw21deko@siena.edu>
2024-02-08 16:38:20 -0500

    Gradient added to the background

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,12 @@ import "./Button.css";
 
 const Button = ({values, className, onClicked}) =>{
 
-    let conditionalClass = "";
+    let conditionalClass = "button";
     if (className === "=") {
         conditionalClass = "equalButton";
     }
     return(
-        
-            <button className={`br4 grow bg-purple white b ${conditionalClass}`}  onClick={onClicked}>{values}</button>
-    
-
+            <button className={`br4 grow white b ${conditionalClass}`}  onClick={onClicked}>{values}</button>
     );
 };
 export default Button;
